fe344b7d74e1a666323245fa661ac4223b935c76
Martin Donovan <martindonovan3@gmail.com>
2024-03-18 13:09:39 -0400

    Initial Commit

---
+++ TransferDBFs.R
@@ -0,0 +1,23 @@
+if (!require("pacman")) install.packages("pacman")
+pacman::p_load(tidyverse, foreign, RCurl, zip, here)
+
+here::here()
+zipFolder <- paste(here(), "/zips", sep = "")
+
+
+AlohaPath <- "C:/Aloha"
+
+
+if (! file.exists(zipFolder)){
+  dir.create(zipFolder)
+} 
+
+folders <- list.dirs(AlohaPath) # this assumes being in the main folder, otherwise specify the path
+folders <- folders[grepl("\d{8}"), folders]
+
+for (f in folders ) {
+  print(f)
+  zip(zipfile = paste(zipFolder, f, sep = ""), files = list.files(f))
+}
+#Aloha.ini
+#UNITNAME=Bones

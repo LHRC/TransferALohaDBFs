1d6c2ec9d44e23482e48b52d93028bdc43378356
Martin Donovan <martindonovan3@gmail.com>
2024-03-25 14:49:02 -0400

    c

--- functions.R
+++ functions.R
@@ -13,6 +13,32 @@ getDBConnection <- function() {
   con
 }
 
+getCurrentFileLocation <-  function()
+{
+  this_file <- commandArgs() %>% 
+    tibble::enframe(name = NULL) %>%
+    tidyr::separate(col=value, into=c("key", "value"), sep="=", fill='right') %>%
+    dplyr::filter(key == "--file") %>%
+    dplyr::pull(value)
+  if (length(this_file)==0)
+  {
+    this_file <- rstudioapi::getSourceEditorContext()$path
+  }
+  return(dirname(this_file))
+}
+
+getExistingImportRecords <- function(){
+  
+}
+
+importRecordIsComplete <- function(){
+  
+}
+
+folderIsValid <- function(){
+  
+}
+
 getEntityId <- function(alohaId, con) {
   query <- paste("select entity_id from entities where aloha_id = ", alohaId, sep = "")
   df <- dbGetQuery(con, query)

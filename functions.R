7d0269ab6d4c14e4f8f7cd5d1fec845df54c16e6
Martin Donovan <martindonovan3@gmail.com>
2024-04-01 06:03:50 -0400

    switched to dbWithTransaction for inserting Aloha grind file records

--- functions.R
+++ functions.R
@@ -13,49 +13,48 @@ getDBConnection <- function() {
   con
 }
 
-getCurrentFileLocation <-  function()
-{
-  this_file <- commandArgs() %>% 
+getCurrentFileLocation <- function() {
+  this_file <- commandArgs() %>%
     tibble::enframe(name = NULL) %>%
-    tidyr::separate(col=value, into=c("key", "value"), sep="=", fill='right') %>%
+    tidyr::separate(col = value, into = c("key", "value"), sep = "=", fill = "right") %>%
     dplyr::filter(key == "--file") %>%
     dplyr::pull(value)
-  if (length(this_file)==0)
-  {
+  if (length(this_file) == 0) {
     this_file <- rstudioapi::getSourceEditorContext()$path
   }
   return(dirname(this_file))
 }
 
-getExistingImportRecords <- function(dataSourceID, con){
+getExistingImportRecords <- function(dataSourceID, con) {
   query <- "select di.data_import_id, di.entity_id, di.import_date, count(i.*) from data_imports di "
-  query <- paste( query, "inner join alohadbf_gnditem i on i.data_import_id ", sep ="")
-  query <- paste( query, "= di.data_import_id where di.import_source_id = ", dataSourceID, sep ="")
-  query <- paste( query, " group by di.data_import_id, di.entity_id, di.import_date")
+  query <- paste(query, "inner join alohadbf_gnditem i on i.data_import_id ", sep = "")
+  query <- paste(query, "= di.data_import_id where di.import_source_id = ", dataSourceID, sep = "")
+  query <- paste(query, " group by di.data_import_id, di.entity_id, di.import_date")
   result <- dbGetQuery(con, query)
 }
 
-importRecordExists <- function(DF, date, entityId){
-  DF %>% filter(entity_id == entityId, import_date == date) %>% count() > 0
+importRecordExists <- function(DF, date, entityId) {
+  DF %>%
+    filter(entity_id == entityId, import_date == date) %>%
+    count() > 0
 }
 
-importRecordIsComplete <- function(){
-  
+importRecordIsComplete <- function() {
+
 }
 
-deleteImportRecord <- function(importId, con){
+deleteImportRecord <- function(importId, con) {
   query1 <- "select table_schema,table_name from information_schema.tables "
   query1 <- paste(query1, "where table_name like 'alohadbf%' ", sep = "")
-  query1 <- paste(query1, "and table_schema not in ('information_schema', 'pg_catalog') " , sep = "")
+  query1 <- paste(query1, "and table_schema not in ('information_schema', 'pg_catalog') ", sep = "")
   query1 <- paste(query1, " and table_type = 'BASE TABLE' order by table_name, table_schema;", sep = "")
   df <- dbGetQuery(con, query1)
-  for(row in 1:nrow(df)){
+  for (row in 1:nrow(df)) {
     tableName <- df[row, "table_name"]
     print(tableName)
   }
-  #query2 <- str::glue("delete from {} where data_import_id = {}")
-  #query3 <- str::glue("delete from data_imports where data_import_id = {}")
-  
+  # query2 <- str::glue("delete from {} where data_import_id = {}")
+  # query3 <- str::glue("delete from data_imports where data_import_id = {}")
 }
 
 getEntityId <- function(alohaId, con) {
@@ -84,16 +83,16 @@ importExistsInDB <- function(date, entity, con) {
 }
 
 
-folderIsValid <- function(folder){
+folderIsValid <- function(folder) {
   isValid <- FALSE
   ini <- paste(folder, "/Aloha.ini", sep = "")
-  line <- paste(folder, "/GNDLINE.dbf", sep = "") 
-  
-  if(file.exists(ini) & file.exists(line) & file.size(line) > 1000){
+  line <- paste(folder, "/GNDLINE.dbf", sep = "")
+
+  if (file.exists(ini) & file.exists(line) & file.size(line) > 1000) {
     iniDF <- read.csv2(paste(folder, "/Aloha.ini", sep = ""), sep = "=", skip = 1, header = FALSE)
-    rNum <- as.integer( iniDF[iniDF$V1 == "UNITNUMBER",]["V2"])
-    if(! is.na(rNum)){
-      isValid = TRUE
+    rNum <- as.integer(iniDF[iniDF$V1 == "UNITNUMBER", ]["V2"])
+    if (!is.na(rNum)) {
+      isValid <- TRUE
     }
   }
   isValid
@@ -102,67 +101,113 @@ folderIsValid <- function(folder){
 insertGrindFiles <- function(grindDate, folder, entityNumber, dataSourceID, con) {
   isError <- FALSE
 
-  tryCatch(
-    expr = {
+
+  dbWithTransaction(
+    con,
+    {
       print(paste("grind date", grindDate))
-      dbBegin(con)
       maxImportIdQuery <- paste("select max(data_import_id) from data_imports where entity_id = ", entityNumber, " and import_source_id = ", dataSourceID)
       previousImportId <- dbGetQuery(con, maxImportIdQuery)[1, 1]
       insertQuery <- paste("insert into data_imports (entity_id, import_source_id, import_date) values (", entityNumber, ", ", dataSourceID, ", '", grindDate, "')", sep = "")
-      print("insert di record")
+      print("insert DI record")
       dataImportRecord <- dbExecute(con, insertQuery)
 
       result <- dbGetQuery(con, maxImportIdQuery)
       dataImportID <- result[1, 1]
       print(paste("prev", previousImportId, "new", dataImportID))
-
-      ##### if import record was inserted
       if (dataImportID > previousImportId) {
-        dbf_files <- list.files(path = folder, pattern = "\\.dbf", ignore.case = TRUE, full.names = TRUE, recursive = FALSE)
-        for (f in dbf_files) {
-          fname <- fs::path_ext_remove(f)
-          fname <- tolower(fs::path_file(fname))
-          table <- paste("alohadbf_", tolower(fname), sep = "")
-          dbf <- read.dbf(f, as.is = T)
-          colnames(dbf) <- tolower(colnames(dbf))
-          dbf <- dbf %>% select(!contains("hash", ignore.case = TRUE))
-          dbf <- dbf %>% select(!contains("ssn", ignore.case = TRUE))
-          dbf <- dbf %>% select(!contains("loyalty", ignore.case = TRUE))
-          dbf <- dbf %>% mutate(grind_date = grindDate, entity_id = entityNumber, data_import_id = dataImportID)
-          dbWriteTable(
-            con,
-            table,
-            dbf,
-            row.names = FALSE,
-            overwrite = FALSE,
-            append = TRUE,
-            field.types = NULL,
-            temporary = FALSE,
-            copy = TRUE
-          )
-        }
+        dbBreak()
+        isError <- TRUE
       }
-    },
-    error = function(e) {
-      print("error")
-      print(e)
-      isError <- TRUE
-      dbRollback(con)
-      # break
-    },
-    warning = function(w) {
-      print("warning")
-      print(w)
-      dbCommit(con)
+
+      dbf_files <- list.files(path = folder, pattern = "\\.dbf", ignore.case = TRUE, full.names = TRUE, recursive = FALSE)
+      for (f in dbf_files) {
+        fname <- fs::path_ext_remove(f)
+        fname <- tolower(fs::path_file(fname))
+        table <- paste("alohadbf_", tolower(fname), sep = "")
+        dbf <- read.dbf(f, as.is = T)
+        colnames(dbf) <- tolower(colnames(dbf))
+        dbf <- dbf %>% select(!contains("hash", ignore.case = TRUE))
+        dbf <- dbf %>% select(!contains("ssn", ignore.case = TRUE))
+        dbf <- dbf %>% select(!contains("loyalty", ignore.case = TRUE))
+        dbf <- dbf %>% mutate(grind_date = grindDate, entity_id = entityNumber, data_import_id = dataImportID)
+        dbWriteTable(
+          con,
+          table,
+          dbf,
+          row.names = FALSE,
+          overwrite = FALSE,
+          append = TRUE,
+          field.types = NULL,
+          temporary = FALSE,
+          copy = TRUE
+        )
+      }
     }
   )
-  if (isError) {
-    print("rollback")
-    dbRollback(con)
-  } else {
-    print("commit")
-    dbCommit(con)
-  }
+  ! isError
+
+  # tryCatch(
+  #   expr = {
+  #     print(paste("grind date", grindDate))
+  #     dbBegin(con)
+  #     maxImportIdQuery <- paste("select max(data_import_id) from data_imports where entity_id = ", entityNumber, " and import_source_id = ", dataSourceID)
+  #     previousImportId <- dbGetQuery(con, maxImportIdQuery)[1, 1]
+  #     insertQuery <- paste("insert into data_imports (entity_id, import_source_id, import_date) values (", entityNumber, ", ", dataSourceID, ", '", grindDate, "')", sep = "")
+  #     print("insert di record")
+  #     dataImportRecord <- dbExecute(con, insertQuery)
+  #
+  #     result <- dbGetQuery(con, maxImportIdQuery)
+  #     dataImportID <- result[1, 1]
+  #     print(paste("prev", previousImportId, "new", dataImportID))
+  #
+  #     ##### if import record was inserted
+  #     if (dataImportID > previousImportId) {
+  #       dbf_files <- list.files(path = folder, pattern = "\\.dbf", ignore.case = TRUE, full.names = TRUE, recursive = FALSE)
+  #       for (f in dbf_files) {
+  #         fname <- fs::path_ext_remove(f)
+  #         fname <- tolower(fs::path_file(fname))
+  #         table <- paste("alohadbf_", tolower(fname), sep = "")
+  #         dbf <- read.dbf(f, as.is = T)
+  #         colnames(dbf) <- tolower(colnames(dbf))
+  #         dbf <- dbf %>% select(!contains("hash", ignore.case = TRUE))
+  #         dbf <- dbf %>% select(!contains("ssn", ignore.case = TRUE))
+  #         dbf <- dbf %>% select(!contains("loyalty", ignore.case = TRUE))
+  #         dbf <- dbf %>% mutate(grind_date = grindDate, entity_id = entityNumber, data_import_id = dataImportID)
+  #         dbWriteTable(
+  #           con,
+  #           table,
+  #           dbf,
+  #           row.names = FALSE,
+  #           overwrite = FALSE,
+  #           append = TRUE,
+  #           field.types = NULL,
+  #           temporary = FALSE,
+  #           copy = TRUE
+  #         )
+  #       }
+  #     }
+  #   },
+  #   error = function(e) {
+  #     print("error")
+  #     print(e)
+  #     isError <- TRUE
+  #     dbRollback(con)
+  #     # break
+  #   },
+  #   warning = function(w) {
+  #     print("warning")
+  #     print(w)
+  #     dbCommit(con)
+  #   }
+  # )
+  # if (isError) {
+  #   print("rollback")
+  #   dbRollback(con)
+  # } else {
+  #   print("commit")
+  #   dbCommit(con)
+  # }
 }
 
 # insertAlohaDBF <- function(date, entity, folder, con) {
